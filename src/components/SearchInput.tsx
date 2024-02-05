13f599a96164f4ecd799bc3f26ea0680a431b241
HiufaiLau <hiufailau.41@gmail.com>
2024-02-05 12:31:33 +0100

    added prop types and cn fucntion in Navbar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { IoSearch } from "react-icons/io5";
 
-type Props = {};
+type Props = {
+  className?: string;
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
+  onSubmit: React.FormEventHandler<HTMLFormElement> | undefined;
+};
 
-export default function SearchInput({}: Props) {
+export default function SearchInput(props: Props) {
   return (
     <div>
       {" "}
-      <form className={"flex relative items-center justify-center h-10"}>
+      <form
+        className={"flex relative items-center justify-center h-10"}
+        onSubmit={props.onSubmit}
+      >
         <input
+          type='text'
+          value={props.value}
+          onChange={props.onChange}
           placeholder='Search location..'
           className='px-4 py-2 w-[230px] border border-gray-300 rounded-l-md focus:outline-none  focus:border-blue-500 h-full'
         />
