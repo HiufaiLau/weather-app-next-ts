e15ceb71d60fc89d21406578f0178ced6fc0ac99
HiufaiLau <hiufailau.41@gmail.com>
2024-02-05 15:33:32 +0100

    added date fn functions to format date string

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@
 import { useQuery } from "react-query";
 import Navbar from "../components/Navbar";
 import axios from "axios";
+import { parseISO, format } from "date-fns";
 
 interface WeatherDetail {
   dt: number;
@@ -63,7 +64,7 @@ interface WeatherData {
 export default function Home() {
   const { isLoading, error, data } = useQuery<WeatherData>("data", async () => {
     const { data } = await axios.get(
-      `https://api.openweathermap.org/data/2.5/find?q=berlin&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
+      `https://api.openweathermap.org/data/2.5/forecast?q=berlin&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
     );
     return data;
     // fetch(
@@ -71,13 +72,27 @@ export default function Home() {
     // ).then((res) => res.json())
   });
 
-  console.log(data);
+  const firstData = data?.list[0];
+  console.log("data", firstData);
 
   if (isLoading) return "Loading...";
 
   return (
     <div className='flex flex-col gap-4 bg-gray-100 min-h-screen'>
       <Navbar />
+      <main className='px-3 max-w-7xl mx-auto flex flex-col gap-9  w-full  pb-10 pt-4'>
+        {/* today data */}
+        <section className='space-y-4'>
+          <div className='space-y-2'>
+            <div className='flex gap-1 items-end text-2xl'>
+              <p>{format(parseISO(firstData?.dt_txt ?? ""), "EEEE")}</p>
+              <p className='text-lg'>
+                ({format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")})
+              </p>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
